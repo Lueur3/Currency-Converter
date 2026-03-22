796f5ddf82f6876465595163c461477a166c3bfc
Lueur3 <sgdm515@gmail.com>
2026-03-22 18:38:52 +0300

    valid funcs

---
+++ src/validator.py
@@ -0,0 +1,22 @@
+def validate_amount(amount: str):
+    try:
+        amnt = float(amount)
+        if amnt <= 0:
+            raise ValueError("The value entered must be greater than or equal to zero.")
+
+        return amnt
+
+    except ValueError:
+        raise ValueError("Invalid value. Please enter a number.")
+
+
+def validate_currency_code(code: str):
+    if len(code) != 3:
+        raise ValueError("The code entered is the wrong length.")
+
+    if not code.isalpha() or not code.isascii():
+        raise ValueError(
+            "The code must consist only of letters from the Latin alphabet"
+        )
+
+    return code
