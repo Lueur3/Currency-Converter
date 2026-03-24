5de2ba471bb091bca7bed59a31bf5d761a8a63d1
Lueur3 <sgdm515@gmail.com>
2026-03-24 11:35:32 +0300

    test validator

---
+++ tests/test_validator.py
@@ -0,0 +1,32 @@
+import pytest
+from src.validator import validate_amount, validate_currency_code
+
+
+def test_valid_type_except():
+    with pytest.raises(ValueError):
+        validate_amount("uuu")
+
+
+@pytest.mark.parametrize("amount, expected_type", [(123, float)])
+def test_valid_type(amount, expected_type):
+    assert isinstance(validate_amount(amount), expected_type)
+
+
+def test_valid_negative_num():
+    with pytest.raises(ValueError):
+        validate_amount("-123")
+
+
+@pytest.mark.parametrize("code", ["uu", "uuuu"])
+def test_valid_code_len(code):
+    with pytest.raises(ValueError, match="The code entered is the wrong length."):
+        validate_currency_code(code)
+
+
+@pytest.mark.parametrize("code", ["u13", "1us", "#$S"])
+def test_valid_code_cont(code):
+    with pytest.raises(
+        ValueError,
+        match="The code must consist only of letters from the Latin alphabet",
+    ):
+        validate_currency_code(code)
